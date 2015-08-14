84b6b1bb75ec6c5734c09ea44d4c856ddd393ea5
Nurha <nurha@projectcodex.co>
2015-08-14 13:50:41 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var item = require('./itemsSold');
+var mostPopularProd = require('./mostPopular')
 
 var app = express();
 var fs = require("fs");
@@ -15,6 +16,11 @@ app.get('/', function (req, res) {
 app.get('/sold', function (req, res) {
 	var productNames = item.productsSold('./Nelisa Sales History.csv');
     res.render('ItemSold', {products: productNames});
+});
+
+app.get('/popular', function (req, res) {
+	var mostPopularProduct = mostPopularProd.mostPopular('./Nelisa Sales History.csv');
+	res.render('mostPopular',{product: mostPopularProduct});
 });
  
 app.listen(3000);
