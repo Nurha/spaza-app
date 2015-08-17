09b4c7950822fe24221e3ae73cf1ef1d412895ec
Nurha <nurha@projectcodex.co>
2015-08-17 13:49:54 +0200

    still in progress

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+
 var item = require('./itemsSold');
 var mostPopularProd = require('./mostPopular')
+var categoryCalculations = require('./categoryCalculations')
+var findMostPopularCategory = require('./findMostPopularCategory')
 
 var app = express();
 var fs = require("fs");
@@ -22,5 +25,22 @@ app.get('/popular', function (req, res) {
 	var mostPopularProduct = mostPopularProd.mostPopular('./Nelisa Sales History.csv');
 	res.render('mostPopular',{product: mostPopularProduct});
 });
+
+app.get('/category', function (req, res){
+	var categoriesNames = categoryCalculations.getSalesPerCategory('./Nelisa Sales History.csv');
+	res.render ('category', {category: categoriesNames});
+});
+
+app.get('/mostPopularCategory', function (req, res){
+	var categoryName = findMostPopularCategory.findMostProfitableCategory('./Nelisa Sales History.csv');
+	res.render('mostPopularCategory',{category: categoryName});
+});
  
-app.listen(3000);
+ var server = app.listen(3000, function () {
+
+     var host = server.address().address;
+     var port = server.address().port;
+
+     console.log('app listening at http://%s:%s', host, port);
+
+   });
