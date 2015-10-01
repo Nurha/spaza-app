6d904edcef394746b764d2505a7fbeba8a33718c
Nurha <nurha@projectcodex.co>
2015-10-01 14:25:21 +0200

    done

---
+++ products.sql
@@ -0,0 +1,22 @@
+INSERT INTO Products(product_name, category_id) VALUES('Milk 1l', 3);
+INSERT INTO Products(product_name, category_id) VALUES('Imasi', 3);
+INSERT INTO Products(product_name, category_id) VALUES('Bread', 7);
+INSERT INTO Products(product_name, category_id) VALUES('Chakalaka Can', 2);
+INSERT INTO Products(product_name, category_id) VALUES('Gold Dish Vegetable Curry Can', 2);
+INSERT INTO Products(product_name, category_id) VALUES('Fanta 500ml', 6);
+INSERT INTO Products(product_name, category_id) VALUES('Coke 500ml', 6);
+INSERT INTO Products(product_name, category_id) VALUES('Cream Soda 500ml', 6);
+INSERT INTO Products(product_name, category_id) VALUES('Iwisa Pap 5kg', 7);
+INSERT INTO Products(product_name, category_id) VALUES('Top Class Soy Mince', 7);
+INSERT INTO Products(product_name, category_id) VALUES('Shampoo 1 litre',8);
+INSERT INTO Products(product_name, category_id) VALUES('Soap Bar', 8);
+INSERT INTO Products(product_name, category_id) VALUES('Bananas - loose', 4);
+INSERT INTO Products(product_name, category_id) VALUES('Apples - loose', 4);
+INSERT INTO Products(product_name, category_id) VALUES('Mixed Sweets 5s', 1);
+INSERT INTO Products(product_name, category_id) VALUES('Heart Chocolates', 1);
+INSERT INTO Products(product_name, category_id) VALUES('Rose (plastic)', 5);
+INSERT INTO Products(product_name, category_id) VALUES('Valentine Cards', 5);
+
+
+
+
