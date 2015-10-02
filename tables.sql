ff7f0c888230f9130122e47b1c74959281ddc29a
Nurha <nurha@projectcodex.co>
2015-10-02 10:30:16 +0200

    done

--- tables.sql
+++ tables.sql
@@ -1,40 +1,41 @@
 CREATE TABLE Categories (
-    category_id int not null auto_increment,
-    category_name char(100),
-    primary key(category_id),
-    CONSTRAINT uc_category_name  UNIQUE (category_name)
-   )ENGINE=InnoDB;
+    cat_id int not null auto_increment,
+    cat_name char(100),
+    primary key(cat_id),
+    CONSTRAINT uc_cat_name  UNIQUE (cat_name)
+    
+);
 
 CREATE TABLE Products (
-    product_id int not null auto_increment,
-    product_name char(100),
-    category_id int(42),
-    primary key(product_id),
-    CONSTRAINT uc_product_name  UNIQUE (product_name)
-    FOREIGN KEY(category_id) REFERENCES Categories(category_id)
-)ENGINE=InnoDB;
+    prod_id int not null auto_increment,
+    prod_name char(100),
+    cat_id int(42),
+    primary key(prod_id),
+    FOREIGN KEY(cat_id) REFERENCES Categories(cat_id),
+    CONSTRAINT uc_prod_name  UNIQUE (prod_name)
+
+);
 
  create table Suppliers (
-     supply_id int not null auto_increment,
+     sply_id int not null auto_increment,
      supply_name char(100),
-     primary key(supply_id)
-)ENGINE=InnoDB;
+     primary key(sply_id)
+);
 
 CREATE TABLE Purchases (
     purchase_id int not null auto_increment,
     name char(100),
     cost_price int(42),
-    product_id int(42),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    prod_id int(42),
+    FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
     primary key(purchase_id)
-)ENGINE=InnoDB;
+);
 
 CREATE TABLE  Sales (
-   sale_id int not null auto_increment,
+   sales_id int not null auto_increment,
     qty int(42),
     sales_price int(42),
-    product_id int(42),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id),
-    primary key(sale_id)
- )ENGINE=InnoDB;
-
+    prod_id int(42),
+    FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
+    primary key(sales_id)
+ );
