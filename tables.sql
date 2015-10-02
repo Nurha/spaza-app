ff0f8ad117ca6231b2be817601e265830d1e0a0d
Nurha <nurha@projectcodex.co>
2015-10-02 13:27:36 +0200

    done

--- tables.sql
+++ tables.sql
@@ -1,41 +1,45 @@
-CREATE TABLE Categories (
-    cat_id int not null auto_increment,
-    cat_name char(100),
-    primary key(cat_id),
+CREATE TABLE `Categories` (
+    `cat_id` int NOT NULL auto_increment,
+    `cat_name` char(100),
+    PRIMARY KEY(cat_id),
     CONSTRAINT uc_cat_name  UNIQUE (cat_name)
     
 );
 
-CREATE TABLE Products (
-    prod_id int not null auto_increment,
-    prod_name char(100),
-    cat_id int(42),
-    primary key(prod_id),
+CREATE TABLE `Products` (
+    `prod_id` int NOT NULL auto_increment,
+    `prod_name` char(100),
+    `cat_id` int(42),
     FOREIGN KEY(cat_id) REFERENCES Categories(cat_id),
+    PRIMARY KEY(prod_id),
     CONSTRAINT uc_prod_name  UNIQUE (prod_name)
 
 );
 
- create table Suppliers (
-     sply_id int not null auto_increment,
-     supply_name char(100),
-     primary key(sply_id)
+ CREATE TABLE `Suppliers` (
+     `sply_id` int NOT NULL auto_increment,
+     `supply_name` char(100),
+     PRIMARY KEY(sply_id)
 );
 
-CREATE TABLE Purchases (
-    purchase_id int not null auto_increment,
-    name char(100),
-    cost_price int(42),
-    prod_id int(42),
+CREATE TABLE `Purchases` (
+    `purchase_id` int NOT NULL auto_increment,
+    `prod_id` int(42),
+    `sply_id` int(42),
+    `date` date NOT NULL,
+    `cost_price` decimal(42,1),
+    `qty` int(42), 
     FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
-    primary key(purchase_id)
+    FOREIGN KEY (sply_id) REFERENCES Suppliers(sply_id),
+    PRIMARY KEY(purchase_id)
 );
 
-CREATE TABLE  Sales (
-   sales_id int not null auto_increment,
-    qty int(42),
-    sales_price int(42),
-    prod_id int(42),
+CREATE TABLE  `Sales` (
+    `sales_id` int NOT NULL auto_increment,
+    `prod_id` int(42),
+    `date` date NOT NULL,
+    `sales_price` decimal(42,1),
+    `qty` int(42),
     FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
-    primary key(sales_id)
+    PRIMARY KEY(sales_id)
  );
