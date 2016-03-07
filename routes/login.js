50185e65af802e9afb1acd1856d3dc1ebf3d941c
Nurha <nurha@projectcodex.co>
2016-03-07 17:57:26 +0200

    working on logging in

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,18 @@
+var bcrypt = require('bcrypt');
+
 exports.userLogin = function(req, res){
 	res.render('login_signup')
 };
+
+// exports.login = function(req, res, next){
+// 	req.getConnection(function(err, connection){
+// 		var input = JSON.parse(JSON.stringify(req.body));
+// 		var data = {
+// 			user_name : input.user_name,
+//       user_password : input.user_password,
+// 			if(err){
+// 				return next(err);
+// 			};
+// 		};
+// 	});
+// };
