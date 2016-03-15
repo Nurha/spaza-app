64944d09e75d58c5f46ba7398ed60d8c23790e62
Nurha <nurha@projectcodex.co>
2016-03-15 14:59:29 +0200

    adding admin privileges

--- routes/categories.js
+++ routes/categories.js
@@ -1,106 +1,108 @@
-exports.showCategories = function(req, res, next){
-	req.getConnection(function(err, connection){
-		if(err) return next(err);
-		connection.query('SELECT * FROM Categories', [], function(err, results){
-			res.render('Categories', {
-				no_categories : results.length===0,
-				categories : results
-			});
-		});
-	});
+exports.showCategories = function(req, res, next) {
+  var admin = req.session.description === 'admin'
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    connection.query('SELECT * FROM Categories', [], function(err, results) {
+      res.render('Categories', {
+        no_categories: results.length === 0,
+        categories: results,
+        admin: admin
+      });
+    });
+  });
 };
 
-exports.showAddCategories = function(req, res){
-	req.getConnection(function(err, connection){
-		if(err) return next(err);
-		connection.query('SELECT * FROM Categories', [], function(err, categories){
-			if(err) return next(err);
-			res.render('addCategories', {
-				categories : categories
-			});
-		});
-	});
+exports.showAddCategories = function(req, res) {
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    connection.query('SELECT * FROM Categories', [], function(err, categories) {
+      if (err) return next(err);
+      res.render('addCategories', {
+        categories: categories
+      });
+    });
+  });
 };
 
-exports.addCategories = function(req, res, next){
-	req.getConnection(function(err,connection){
-		if(err) return next(err);
-		var input = JSON.parse(JSON.stringify(req.body));
-		var data = {
-			category_id : input.category_id,
-			category_name : input.category_name
-		};
-		connection.query('insert into Categories set ?', data, function(err, results) {
-  			if (err) return next(err);
-			res.redirect('/categories');
-		});
-	});
+exports.addCategories = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    var input = JSON.parse(JSON.stringify(req.body));
+    var data = {
+      category_id: input.category_id,
+      category_name: input.category_name
+    };
+    connection.query('insert into Categories set ?', data, function(err, results) {
+      if (err) return next(err);
+      res.redirect('/categories');
+    });
+  });
 };
 
-exports.getCategories = function(req, res, next){
-	var category_id = req.params.category_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT * FROM Categories WHERE category_id = ?', [category_id], function(err, rows){
-			if(err) return next(err);
-			res.render('editCategories', {
-				data : rows[0]
-			});
-		});
-	});
+exports.getCategories = function(req, res, next) {
+  var category_id = req.params.category_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT * FROM Categories WHERE category_id = ?', [category_id], function(err, rows) {
+      if (err) return next(err);
+      res.render('editCategories', {
+        data: rows[0]
+      });
+    });
+  });
 };
 
-exports.updateCategories = function(req, res, next){
-	var data = JSON.parse(JSON.stringify(req.body));
-  	var category_id = req.params.category_id;
-  	req.getConnection(function(err, connection){
-		connection.query('UPDATE Categories SET ? WHERE category_id = ?', [data, category_id], function(err, rows){
-    		if (err) next(err);
-          	res.redirect('/categories');
-    	});
-	});
+exports.updateCategories = function(req, res, next) {
+  var data = JSON.parse(JSON.stringify(req.body));
+  var category_id = req.params.category_id;
+  req.getConnection(function(err, connection) {
+    connection.query('UPDATE Categories SET ? WHERE category_id = ?', [data, category_id], function(err, rows) {
+      if (err) next(err);
+      res.redirect('/categories');
+    });
+  });
 };
 
-exports.delete = function(req, res, next){
-	var category_id = req.params.category_id;
-	req.getConnection(function(err, connection){
-		connection.query('DELETE FROM Categories WHERE category_id = ?', [category_id], function(err,rows){
-			if(err) return next(err);
-			res.redirect('/categories');
-		});
-	});
+exports.delete = function(req, res, next) {
+  var category_id = req.params.category_id;
+  req.getConnection(function(err, connection) {
+    connection.query('DELETE FROM Categories WHERE category_id = ?', [category_id], function(err, rows) {
+      if (err) return next(err);
+      res.redirect('/categories');
+    });
+  });
 };
 
-exports.categoriesPopularity = function(req, res, next){
-	var categories_id = req.params.categories_id;
-	req.getConnection(function(err, connection){
-		connection.query( 'SELECT Categories.category_name, sum( Sales.Qty ) AS qty FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id INNER JOIN Categories ON Products.Category_id = Categories.category_id GROUP BY Categories.category_name ORDER BY qty DESC', [] , function(err, results) {
-			if(err) return next(err);
-			res.render('categoriesPopularity', {
-				categoriesPopularity: results
-			});
-		});
-	});
+exports.categoriesPopularity = function(req, res, next) {
+  var categories_id = req.params.categories_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT Categories.category_name, sum( Sales.Qty ) AS qty FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id INNER JOIN Categories ON Products.Category_id = Categories.category_id GROUP BY Categories.category_name ORDER BY qty DESC', [], function(err, results) {
+      if (err) return next(err);
+      res.render('categoriesPopularity', {
+        categoriesPopularity: results
+      });
+    });
+  });
 };
 
-exports.categoryEarnings = function(req, res, next){
-	//var category_id = req.params.category_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT Categories.category_name, SUM(Sales.qty * Sales.sales_price) AS earnings FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id INNER JOIN Categories ON Categories.category_id = Products.category_id GROUP BY Categories.category_name ORDER BY earnings DESC', [], function(err, result){
-			if(err) return next(err);
-			res.render('categoryEarnings',{
-				categoryEarnings: result
-			});
-		});
-	});
+exports.categoryEarnings = function(req, res, next) {
+  //var category_id = req.params.category_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT Categories.category_name, SUM(Sales.qty * Sales.sales_price) AS earnings FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id INNER JOIN Categories ON Categories.category_id = Products.category_id GROUP BY Categories.category_name ORDER BY earnings DESC', [], function(err, result) {
+      if (err) return next(err);
+      res.render('categoryEarnings', {
+        categoryEarnings: result
+      });
+    });
+  });
 };
 
-exports.categoryProfits = function(req, res, next){
-	req.getConnection(function(err,connection){
-		connection.query('SELECT category_name, supplier_name, SUM(sales_price - cost_price) AS profit FROM Products, Sales, Purchases, Suppliers, Categories	WHERE Products.product_id = Sales.product_id AND Products.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id AND Products.category_id = Categories.category_id GROUP BY category_name ORDER BY profit DESC;', [], function(err, result){
-			if(err) return next(err);
-			res.render('categoryProfits',{
-				categoryProfits : result
-			});
-		});
-	});
+exports.categoryProfits = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT category_name, supplier_name, SUM(sales_price - cost_price) AS profit FROM Products, Sales, Purchases, Suppliers, Categories	WHERE Products.product_id = Sales.product_id AND Products.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id AND Products.category_id = Categories.category_id GROUP BY category_name ORDER BY profit DESC;', [], function(err, result) {
+      if (err) return next(err);
+      res.render('categoryProfits', {
+        categoryProfits: result
+      });
+    });
+  });
 };
--- routes/products.js
+++ routes/products.js
@@ -1,120 +1,119 @@
-exports.home = function(req, res){
-	res.render('home')
+exports.home = function(req, res) {
+  res.render('home')
 }
 
-exports.show = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err) return next(err);
-		connection.query('SELECT Products.product_id, Products.product_name, Categories.category_name FROM Products INNER JOIN Categories ON Categories.category_id = Products.category_id ORDER BY product_id', [], function(err, results) {
-
-        		if (err) return next(err);
-    			res.render( 'Products', {
-					no_products : results.length === 0,
-					products : results
-
-    			});
-
-		});
-	});
-};
-
-exports.showAdd = function(req, res){
-	req.getConnection(function(err, connection){
-		if (err) return next(err);
-		connection.query('SELECT * from Categories', [], function(err, categories) {
-			res.render('addProducts', {
-				categories: categories
+exports.show = function(req, res, next) {
+	var admin = req.session.description === 'admin'
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    connection.query('SELECT Products.product_id, Products.product_name, Categories.category_name FROM Products INNER JOIN Categories ON Categories.category_id = Products.category_id ORDER BY product_id', [], function(err, results) {
+			if (err) return next(err);
+      res.render('Products', {
+        no_products: results.length === 0,
+        products: results,
+				admin : admin
 			});
 		});
-	});
+  });
 };
 
-exports.add = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err) return next(err);
-		var input = JSON.parse(JSON.stringify(req.body));
-		var data = {
-      		product_name : input.product_name,
-      		category_id : input.category_id
-  		};
-		connection.query('insert into Products set ?', data, function(err, results) {
-  			if (err) return next(err);
-			res.redirect('/products');
-		});
-	});
-};
-
-exports.get = function(req, res, next){
-	var product_id = req.params.product_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT * FROM Products WHERE product_id = ?', [product_id], function(err, rows){
-			connection.query('SELECT * FROM Categories', [], function(err,results){
-				if(err) return next(err);
-				res.render('edit',{
-					page_title:"Edit Products - Node.js",
-					data : rows[0],
-					categories : results
-				});
-			});
-		});
-	});
-};
-
-exports.update = function(req, res, next){
-	var data = JSON.parse(JSON.stringify(req.body));
-    var product_id = req.params.product_id;
-    req.getConnection(function(err, connection){
-		connection.query('UPDATE Products SET ? WHERE product_id = ?', [data, product_id], function(err, rows){
-    		if (err) next(err);
-            res.redirect('/products');
-    	});
+exports.showAdd = function(req, res) {
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    connection.query('SELECT * from Categories', [], function(err, categories) {
+      res.render('addProducts', {
+        categories: categories
+      });
     });
+  });
+};
+
+exports.add = function(req, res, next) {
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+    var input = JSON.parse(JSON.stringify(req.body));
+    var data = {
+      product_name: input.product_name,
+      category_id: input.category_id
+    };
+    connection.query('insert into Products set ?', data, function(err, results) {
+      if (err) return next(err);
+      res.redirect('/products');
+    });
+  });
+};
+
+exports.get = function(req, res, next) {
+  var product_id = req.params.product_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT * FROM Products WHERE product_id = ?', [product_id], function(err, rows) {
+      connection.query('SELECT * FROM Categories', [], function(err, results) {
+        if (err) return next(err);
+        res.render('edit', {
+          page_title: "Edit Products - Node.js",
+          data: rows[0],
+          categories: results
+        });
+      });
+    });
+  });
+};
+
+exports.update = function(req, res, next) {
+  var data = JSON.parse(JSON.stringify(req.body));
+  var product_id = req.params.product_id;
+  req.getConnection(function(err, connection) {
+    connection.query('UPDATE Products SET ? WHERE product_id = ?', [data, product_id], function(err, rows) {
+      if (err) next(err);
+      res.redirect('/products');
+    });
+  });
 };
 
 
 
-exports.delete = function(req, res, next){
-	var product_id = req.params.product_id;
-	req.getConnection(function(err, connection){
-		connection.query('DELETE FROM Products WHERE product_id = ?', [product_id], function(err,rows){
-			if(err) return next(err);
-			res.redirect('/products');
-		});
-	});
+exports.delete = function(req, res, next) {
+  var product_id = req.params.product_id;
+  req.getConnection(function(err, connection) {
+    connection.query('DELETE FROM Products WHERE product_id = ?', [product_id], function(err, rows) {
+      if (err) return next(err);
+      res.redirect('/products');
+    });
+  });
 };
 
-exports.productPopularity = function(req, res, next){
-	var product_id = req.params.product_id;
-	req.getConnection(function(err, connection){
-		connection.query( 'SELECT product_name, supplier_name, Sum(Sales.qty) AS qty FROM Products, Sales, Purchases, Suppliers, WHERE Products.product_id = Sales.product_id AND Products.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id GROUP BY Products.product_name ORDER BY qty DESC', [], function(err, results) {
-			if(err) return next(err);
-			res.render('productPopularity', {
-				productPopularity: results
-			});
-		});
-	});
+exports.productPopularity = function(req, res, next) {
+  var product_id = req.params.product_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT product_name, supplier_name, Sum(Sales.qty) AS qty FROM Products, Sales, Purchases, Suppliers, WHERE Products.product_id = Sales.product_id AND Products.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id GROUP BY Products.product_name ORDER BY qty DESC', [], function(err, results) {
+      if (err) return next(err);
+      res.render('productPopularity', {
+        productPopularity: results
+      });
+    });
+  });
 };
 
-exports.productEarnings = function(req, res, next){
-	var product_id = req.params.product_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT Products.product_name, SUM(Sales.qty * Sales.sales_price) AS earnings FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id GROUP BY Products.product_name ORDER BY earnings DESC',[], function(err, result){
-			if(err) return(err);
-			res.render('productEarnings',{
-					productEarnings: result
-			});
-		});
-	});
+exports.productEarnings = function(req, res, next) {
+  var product_id = req.params.product_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT Products.product_name, SUM(Sales.qty * Sales.sales_price) AS earnings FROM Sales INNER JOIN Products ON Sales.product_id = Products.product_id GROUP BY Products.product_name ORDER BY earnings DESC', [], function(err, result) {
+      if (err) return (err);
+      res.render('productEarnings', {
+        productEarnings: result
+      });
+    });
+  });
 };
 
-exports.productProfits = function(req, res, next){
-	var product_id = req.params.prroduct_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT product_name, supplier_name, (sales_price-cost_price) AS Profit FROM Products, Sales, Purchases, Suppliers WHERE Products.product_id = Sales.product_id And Sales.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id GROUP BY product_name ORDER BY Profit DESC', [], function(err, result){
-			if(err) return(err);
-			res.render('productProfits',{
-				productProfits : result
-			});
-		});
-	});
+exports.productProfits = function(req, res, next) {
+  var product_id = req.params.prroduct_id;
+  req.getConnection(function(err, connection) {
+    connection.query('SELECT product_name, supplier_name, (sales_price-cost_price) AS Profit FROM Products, Sales, Purchases, Suppliers WHERE Products.product_id = Sales.product_id And Sales.product_id = Purchases.product_id AND Purchases.supplier_id = Suppliers.supplier_id GROUP BY product_name ORDER BY Profit DESC', [], function(err, result) {
+      if (err) return (err);
+      res.render('productProfits', {
+        productProfits: result
+      });
+    });
+  });
 };
